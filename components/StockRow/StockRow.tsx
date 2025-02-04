2346129f86da45d851e1aba9950ae2b68d777adf
Erick Alvarez <erick.alvarez@live.com>
2025-02-03 18:17:40 -0800

    feat: refactor Stock row styles

--- components/StockRow/StockRow.tsx
+++ components/StockRow/StockRow.tsx
@@ -8,36 +8,21 @@ interface StockRowProps extends ViewProps {
 }
 
 export default function StockRow({ stock, ...restProps }: StockRowProps) {
-  // console.log({ stock });
   return (
-    <View {...restProps} style={{ paddingVertical: 8 }}>
-      <View
-        id="dataContainer"
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-          gap: 4,
-          // paddingVertical: 8,
-        }}
-      >
-        <Text style={{ fontSize: 20 }}>{stock.name}</Text>
-        <Text style={{ fontSize: 16, color: "gray" }}>({stock.symbol})</Text>
+    <View {...restProps} style={styles.root}>
+      <View id="dataContainer" style={styles.dataContainer}>
+        <Text style={styles.nameLabel}>{stock.name}</Text>
+        <Text style={styles.symbolLabeL}>({stock.symbol})</Text>
       </View>
-      <View
-        id="analyticsContainer"
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-          gap: 4,
-          // paddingVertical: 8,
-        }}
-      >
-        <Text style={{ fontSize: 16 }}>Price: {formatPrice(stock.price)}</Text>
+      <View id="analyticsContainer" style={styles.analyticsContainer}>
+        <Text style={styles.priceLabeL}>Price: {formatPrice(stock.price)}</Text>
         <Text
-          style={{
-            fontSize: 16,
-            color: stock.daily_change < 0 ? "red" : "green",
-          }}
+          style={[
+            styles.dailyChange,
+            stock.daily_change < 0
+              ? styles.dailyChangeNegative
+              : styles.dailyChangePositive,
+          ]}
         >
           ({stock.daily_change})
         </Text>
@@ -47,20 +32,36 @@ export default function StockRow({ stock, ...restProps }: StockRowProps) {
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
+  root: {
+    paddingVertical: 8,
+  },
+  dataContainer: {
     flexDirection: "row",
     alignItems: "center",
-    gap: 8,
+    gap: 4,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  analyticsContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 4,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
+  nameLabel: {
+    fontSize: 20,
+  },
+  symbolLabeL: {
+    fontSize: 16,
+    color: "gray",
+  },
+  priceLabeL: {
+    fontSize: 16,
+  },
+  dailyChange: {
+    fontSize: 16,
+  },
+  dailyChangePositive: {
+    color: "green",
+  },
+  dailyChangeNegative: {
+    color: "red",
   },
 });
